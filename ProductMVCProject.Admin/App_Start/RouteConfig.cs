248c2f664385244bd8793cec0717f0c9afb24693
Adem AKKUŞ <32845253+ademakkus@users.noreply.github.com>
2020-05-19 18:27:18 +0300

    Add files via upload

---
+++ ProductMVCProject.Admin/App_Start/RouteConfig.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace ProductMVCProject.Admin
+{
+	public class RouteConfig
+		//bu sınıf ve metotları Global.asax da tanımlı
+	{
+		public static void RegisterRoutes(RouteCollection routes)
+		{
+			routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+			routes.MapRoute(
+					name: "Default",
+					url: "{controller}/{action}/{id}",
+					defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+			);
+		}
+	}
+}
